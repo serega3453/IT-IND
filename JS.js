fc68bf936071d3b07ab8e39d294af01e8dec0bb0
serega3453 <107370028+serega3453@users.noreply.github.com>
2022-06-13 03:42:17 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,11 +1,13 @@
 document.getElementById("prop").onclick = function()
 {
 alert(`Clicked property JS button`);
+getElementById("status").innerHTML = "You pressed second button!";
 };
 
 function handler() 
 {
 alert('Clicked listened JS button');
+getElementById("status").innerHTML = "You pressed third button!";
 }
 
 const input = document.getElementById("listened_button");
